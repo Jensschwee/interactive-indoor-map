f187576ec02285694fd26a7799d0ce1f5684649c
Jens Hjort Schwee <jensschwee@msn.com>
2016-02-29 13:47:04 +0100

    new icon for temp

--- Interactive Indoor Map/Website/scripts/MenuButtons.js
+++ Interactive Indoor Map/Website/scripts/MenuButtons.js
@@ -4,11 +4,13 @@ function OnFloorLevelButtonClick() {
     if (geoMap != null) {
         geoMap.removeLayer(geojson);
     }
-    PageMethods.DrawFloor(currentFloorLevel, onSuccess);
-
     function onSuccess(response, userContext, methodName) {
         callBackMethodsToDraw(response);
     }
+
+    PageMethods.DrawFloor(currentFloorLevel, onSuccess);
+
+    
 }
 
 function CreateButtons() {
@@ -17,26 +19,28 @@ function CreateButtons() {
     }).addTo(geoMap);
     //https://github.com/brunob/leaflet.fullscreen
 
-    L.easyButton('-1;', function () {
+    L.easyButton('&#45;&#49;', function () {
         currentFloorLevel = -1;
         OnFloorLevelButtonClick();
     }, { position: 'bottomleft' }).addTo(geoMap);
 
-    L.easyButton('0;', function () {
+    L.easyButton('&#48;', function () {
         currentFloorLevel = 0;
         OnFloorLevelButtonClick();
     }, { position: 'bottomleft' }).addTo(geoMap);
-    L.easyButton('1;', function () {
+    L.easyButton('&#49;', function () {
         currentFloorLevel = 1;
         OnFloorLevelButtonClick();
     }, { position: 'bottomleft' }).addTo(geoMap);
-    L.easyButton('2;', function () {
+    L.easyButton('&#50;', function () {
         currentFloorLevel = 2;
         OnFloorLevelButtonClick();
     }, { position: 'bottomleft' }).addTo(geoMap);
 
-    L.easyButton('Temp', function () {
+    var imgTemp = '<img src="Images/temperature.png" width="25" height="25"style=""/>';
+
+    L.easyButton(imgTemp, function () {
         ViewTemperature();
-    }, { position: 'bottomleft' }).addTo(geoMap);
+    }, { position: 'bottomleft', paddingleft: "0px"}).addTo(geoMap);
 
 }
