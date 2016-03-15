ee170b21cf4d702b969901b8cc9d11fd7f59c7e2
Kyzaq <kyzaq@github.com>
2016-03-15 10:39:53 +0100

    UpdateViewTimer disabled for now

--- Interactive Indoor Map/Website/scripts/UpdateViewTimer.js
+++ Interactive Indoor Map/Website/scripts/UpdateViewTimer.js
@@ -1,6 +1,6 @@
-﻿var int = 0;
-setInterval(function () {
-    //drawFloorInfoBox();
-    int++;
-    console.log(int);
-}, 3000);
+﻿//var int = 0;
+//setInterval(function () {
+//    //drawFloorInfoBox();
+//    int++;
+//    console.log(int);
+//}, 3000);
