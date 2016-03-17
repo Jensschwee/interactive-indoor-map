ee7909478b10f87a19032608698f4d02eb709edb
Jens Hjort Schwee <jensschwee@msn.com>
2016-03-17 12:11:10 +0100

    Oprydtning

--- Interactive Indoor Map/Domain/Sensors/SensorLightActivated.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Sensors
-{
-    class SensorLightActivated : Sensor
-    {
-        public bool Activated { get; set; }
-
-        public SensorLightActivated(string name) : base(name) { }
-    }
-}
