8238c46868cbddb0140be88e3cbbe7e29d28fd5f
Jens Hjort Schwee <jensschwee@msn.com>
2016-03-17 11:04:56 +0100

    EF NotMapped

--- Interactive Indoor Map/Website/App_Code/BO/Room.cs
+++ Interactive Indoor Map/Website/App_Code/BO/Room.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Security.AccessControl;
@@ -14,48 +15,60 @@ namespace Website.BO
 
         public string Alias { get; set; }
 
-        //public RoomType Type { get; set; }
+        public RoomType Type { get; set; }
 
         public Area Area { get; set; }
 
         public double SurfaceArea { get; set; }
 
+        [NotMapped]
         public bool Light { get; set; }
-        
+
+        [NotMapped]
         public int Lumen { get; set; }
 
         public int LumenMax { get; set; }
-        
+
+        [NotMapped]
         public bool Motion { get; set; }
-        
+
+        [NotMapped]
         public double Temperature { get; set; }
 
         public double TemperatureMax { get; set; }
-        
+
+        [NotMapped]
         public int CO2 { get; set; }
 
         public int CO2Max { get; set; }
-        
+
+        [NotMapped]
         public int Occupants { get; set; }
 
         public int OccupantsMax { get; set; }
-        
+
+        [NotMapped]
         public double VentilationConsumption { get; set; }
 
+        [NotMapped]
         public double VentilationConsumptionMax { get; set; }
 
+        [NotMapped]
         public double LightConsumption { get; set; }
-
+        
         public double LightConsumptionMax { get; set; }
 
+        [NotMapped]
         public double HardwareConsumption { get; set; }
         
         public double HardwareConsumptionMax { get; set; }
 
+        [NotMapped]
         public double OtherConsumption { get; set; }
 
         public double OtherConsumptionMax { get; set; }
 
+        [NotMapped]
         public double TotalPowerConsumption => VentilationConsumption + LightConsumption + HardwareConsumption + OtherConsumption;
 
         public double TotalPowerConsumptionMax { get; set; }
