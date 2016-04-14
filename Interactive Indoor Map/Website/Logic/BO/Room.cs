b7146caff664dd1ef54844b4706e3af04f061f2a
Jens Hjort Schwee <jensschwee@msn.com>
2016-04-14 10:45:53 +0200

    Rooms for floor 1

--- Interactive Indoor Map/Website/Logic/BO/Room.cs
+++ Interactive Indoor Map/Website/Logic/BO/Room.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,9 @@ namespace Website.Logic.BO
     public abstract class Room
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
         public string Name { get; set; }
         public string Alias { get; set; }
         public RoomType RoomType { get; set; }
