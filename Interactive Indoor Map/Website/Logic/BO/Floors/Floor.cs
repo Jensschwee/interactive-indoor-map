6e9d3a92d061f7f857492f0c7f3a84b0f9572059
Kyzaq <kyzaq@github.com>
2016-04-28 13:55:29 +0200

    Refactoring

---
+++ Interactive Indoor Map/Website/Logic/BO/Floors/Floor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using Website.Logic.BO.Rooms;
+using Website.Logic.BO.Utility;
+
+namespace Website.Logic.BO
+{
+    public abstract class Floor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int FloorLevel { get; set; }
+
+        public string FloorName { get; set; }
+    
+        public List<Room> Rooms { get; set; }
+    
+        public double SurfaceArea { get; set; }
+
+        public Endpoints Endpoints { get; set; }
+
+        [NotMapped]
+        public double MaxTemperature => (Rooms.Where(room => room.GetType() == typeof(LiveRoom)).Cast<LiveRoom>().Sum(room => room.MaxTemperature) / Rooms.Count(room => room.GetType() == typeof(LiveRoom)));
+
+        [NotMapped]
+        public double MinTemperature => (Rooms.Where(room => room.GetType() == typeof(LiveRoom))
+                    .Cast<LiveRoom>()
+                    .Sum(room => room.MinTemperature) /
+            Rooms.Count(room => room.GetType() == typeof(LiveRoom)));
+
+        [NotMapped]
+        public double MaxCO2 => (Rooms.Where(room => room.GetType() == typeof(LiveRoom))
+                    .Cast<LiveRoom>()
+                    .Sum(room => room.MaxCO2) /
+            Rooms.Count(room => room.GetType() == typeof(LiveRoom)));
+
+        [NotMapped]
+        public double MinCO2 => (Rooms.Where(room => room.GetType() == typeof(LiveRoom))
+                    .Cast<LiveRoom>()
+                    .Sum(room => room.MinCO2) /
+            Rooms.Count(room => room.GetType() == typeof(LiveRoom)));
+
+        [NotMapped]
+        public double MaxLumen => (Rooms.Where(room => room.GetType() == typeof(LiveRoom))
+                    .Cast<LiveRoom>()
+                    .Sum(room => room.MaxLumen) / Convert.ToDouble(Rooms.Count(room => room.GetType() == typeof(LiveRoom))));
+
+        public double MinLumen = 0;
+
+        public double MaxHardwareConsumption { get; set; }
+        public double MinHardwareConsumption = 0;
+
+        public double MaxLightConsumption { get; set; }
+        public double MinLightConsumption = 0;
+
+
+        public double MaxVentilationConsumption { get; set; }
+        public double MinVentilationConsumption = 0;
+
+        public double OtherConsumptionMax { get; set; }
+        public double OtherConsumptionMin = 0;
+
+        [NotMapped]
+        public double MaxTotalPowerConsumption => MaxVentilationConsumption + MaxLightConsumption + MaxHardwareConsumption + OtherConsumptionMax;
+        [NotMapped]
+        public double MinTotalPowerConsumption => MinVentilationConsumption + MinLightConsumption + MinHardwareConsumption + OtherConsumptionMin;
+
+
+        public double MaxHotWaterConsumption { get; set; }
+        public double MinHotWaterConsumption = 0;
+
+        public double MaxColdWaterConsumption { get; set; }
+        public double MinColdWaterConsumption = 0;
+
+        [NotMapped]
+        public double MaxWifiClients => (Rooms.Where(room => room.GetType() == typeof(LiveRoom)).Cast<LiveRoom>().Sum(room => room.MaxWifiClients));
+    }
+}
---
+++ Interactive Indoor Map/Website/Logic/BO/Rooms/SensorRoom.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Website.Logic.BO.Utility;
+
+namespace Website.Logic.BO
+{
+    public abstract class SensorRoom : Room
+    {
+        public Endpoints Endpoints { get; set; }
+
+        public Corners Corners { get; set; }
+
+        public double SurfaceArea { get; set; }
+        
+        public double MaxTemperature { get; set; }
+        public double MinTemperature = 15;
+
+        public int MaxCO2 { get; set; }
+        public int MinCO2 = 0;
+        
+        public int MaxLumen { get; set; }
+        public int MinLumen = 0;
+
+        public double MaxHardwareConsumption { get; set; }
+        public double MinHardwareConsumption = 0;
+
+        public double MaxLightConsumption { get; set; }
+        public double MinLightConsumption = 0;
+
+        public double MaxVentilationConsumption { get; set; }
+        public double VentilationConsumptionMin = 0;
+
+        public double MaxOtherConsumption { get; set; }
+        public double MinOtherConsumption = 0;
+
+        [NotMapped]
+        public double MaxTotalPowerConsumption => MaxVentilationConsumption + MaxLightConsumption + MaxHardwareConsumption + MaxOtherConsumption;
+
+        [NotMapped]
+        public double MinTotalPowerConsumption => VentilationConsumptionMin + MinLightConsumption + MinHardwareConsumption + MinOtherConsumption;
+
+        public int MaxOccupants { get; set; }
+        public int MinOccupants = 0;
+
+        public int MaxWifiClients { get; set; }
+        public int MinWifiClients = 0;
+    }
+}
